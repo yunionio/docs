c85038434800cadaeb948c647ebd9b1bcd98d4b9
Zexi Li <zexi.li@qq.com>
2021-05-19 12:33:51 +0800

    feat: add build workflows

---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ "$DEBUG" == "true" ]]; then
+    set -ex
+    export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+fi
+
+CUR_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+pushd $(dirname "$0")/.. > /dev/null
+pushd themes > /dev/null
+if [ ! -d "docsy" ]; then
+    git clone --recurse-submodules --depth 1 https://github.com/google/docsy.git
+else
+    pushd docsy > /dev/null
+    if [ ! -d "assets/vendor/bootstrap/dist" ]; then
+        git pull --recurse-submodules --depth 1
+    fi
+    popd > /dev/null
+fi
+popd > /dev/null
+
+npm install
